be00664646dd4ef69b072f7f6de14701bf472f89
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2024-06-17 12:17:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@
 
 from setuptools import setup, find_packages  # noqa: H301
 
-NAME = "Wardley_API_Python_SDK"
-VERSION = "0.3.0"
+NAME = "WardleyMapsCrew_API_Python_SDK"
+VERSION = "0.4.0"
 # To install the library, run the following
 #
 # python setup.py install
